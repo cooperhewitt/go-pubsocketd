49a1e29a1139f138dec93f5d8f188a9f85ef1588
copea <copea@si.edu>
2014-08-04 16:00:26 +0000

    make logging work, basically

--- pubsocketd.go
+++ pubsocketd.go
@@ -6,17 +6,17 @@ import (
 	"flag"
 	"fmt"
 	"gopkg.in/redis.v1"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
-	"io"
 	"strings"
 )
 
 var (
-    	psdLogger                    *log.Logger
-	// psdLogfile		     string
+	logger                    *log.Logger
+	logFile                   string
 	redisHost                 string
 	redisPort                 int
 	redisChannel              string
@@ -89,26 +89,26 @@ func pubsocketdHandshake(config *websocket.Config, req *http.Request) (err error
 	realIP := headers.Get("X-Real-IP")
 
 	if origin == "" {
-		psdLogger.Printf("[%s][%s][handshake] missing origin", realIP, remoteAddr)
-		return log.Printf("missing origin")
+		logger.Printf("[%s][%s][handshake] missing origin", realIP, remoteAddr)
+		return fmt.Errorf("missing origin")
 	}
 
 	parsed, err := url.Parse(origin)
 
 	if err != nil {
-		psdLogger.Printf("[%s][%s][handshake] failed to parse origin, %v", realIP, remoteAddr, origin)
-		return log.Printf("failed to parse origin")
+		logger.Printf("[%s][%s][handshake] failed to parse origin, %v", realIP, remoteAddr, origin)
+		return fmt.Errorf("failed to parse origin")
 	}
 
 	// See above inre: config.Origin being/becoming a list of url.URLs
 	// (20140727/straup)
 
 	if parsed.String() != config.Origin.String() {
-		psdLogger.Printf("[%s][%s][handshake] invalid origin, expected %v but got %v", realIP, remoteAddr, config.Origin, parsed)
-		return log.Printf("invalid origin")
+		logger.Printf("[%s][%s][handshake] invalid origin, expected %v but got %v", realIP, remoteAddr, config.Origin, parsed)
+		return fmt.Errorf("invalid origin")
 	}
 
-	log.Printf("[%s][%s][handshake] OK", realIP, remoteAddr)
+	logger.Printf("[%s][%s][handshake] OK", realIP, remoteAddr)
 	return
 }
 
@@ -119,13 +119,13 @@ func pubSubHandler(ws *websocket.Conn) {
 
 	realIP := headers.Get("X-Real-IP")
 
-	log.Printf("[%s][%s][request] OK", realIP, remoteAddr)
+	logger.Printf("[%s][%s][request] OK", realIP, remoteAddr)
 
 	pubsubClient := redisClient.PubSub()
 	defer pubsubClient.Close()
 
 	if err := pubsubClient.Subscribe(redisChannel); err != nil {
-		log.Printf("[%s][%s][error] failed to subscribe to pubsub channel %v, because %s", realIP, remoteAddr, redisChannel, err)
+		logger.Printf("[%s][%s][error] failed to subscribe to pubsub channel %v, because %s", realIP, remoteAddr, redisChannel, err)
 		ws.Close()
 		return
 	}
@@ -144,17 +144,17 @@ func pubSubHandler(ws *websocket.Conn) {
 			bytes_blob := []byte(msg.Payload)
 
 			if err := json.Unmarshal(bytes_blob, &json_blob); err != nil {
-				log.Printf("[%s][%s][error] failed to parse JSON %v, because %v", realIP, remoteAddr, msg.Payload, err)
+				logger.Printf("[%s][%s][error] failed to parse JSON %v, because %v", realIP, remoteAddr, msg.Payload, err)
 				continue
 			}
 
 			if err := websocket.JSON.Send(ws, json_blob); err != nil {
-				log.Printf("[%s][%s][error] failed to send JSON, because %v", realIP, remoteAddr, err)
+				logger.Printf("[%s][%s][error] failed to send JSON, because %v", realIP, remoteAddr, err)
 				ws.Close()
 				break
 			}
 
-			log.Printf("[%s][%s][send] OK", realIP, remoteAddr)
+			logger.Printf("[%s][%s][send] OK", realIP, remoteAddr)
 		}
 	}
 }
@@ -166,41 +166,37 @@ func main() {
 	flag.StringVar(&websocketRoute, "ws-route", "/", "WebSocket route")
 	flag.StringVar(&websocketAllowableOrigins, "ws-origin", "", "WebSocket allowable origin(s)")
 	flag.BoolVar(&websocketAllowInsecure, "ws-insecure", false, "Allow WebSocket server to run in insecure mode")
-
 	flag.StringVar(&redisHost, "rs-host", "127.0.0.1", "Redis host")
 	flag.IntVar(&redisPort, "rs-port", 6379, "Redis port")
 	flag.StringVar(&redisChannel, "rs-channel", "pubsocketd", "Redis channel")
+	flag.StringVar(&logFile, "ps-log-file", "", "Log requests to this file")
 
 	flag.Parse()
 
-	output := "file.txt"
-	file, err := os.OpenFile(output, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if logFile != "" {
+		file, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
 
-	if err != nil {
-	   panic(err)
-	}
+		if err != nil {
+			panic(err)
+		}
 
-	multi := io.MultiWriter(file, os.Stdout)
-
-	psdLogger := log.New(multi, "[pubsocketd] ", log.Ldate|log.Ltime|log.Lshortfile)
-
-	if websocketAllowableOrigins == "" {
-		err, _ := psdLogger.Fatalf("Missing allowable Origin (-ws-origin=http://example.com)")
-		panic(err)
+		multi := io.MultiWriter(file, os.Stdout)
+		logger = log.New(multi, "[pubsocketd] ", log.Ldate|log.Ltime|log.Lshortfile)
+	} else {
+		multi := io.MultiWriter(os.Stdout)
+		logger = log.New(multi, "[pubsocketd] ", log.Ldate|log.Ltime|log.Lshortfile)
 	}
 
 	if !websocketAllowInsecure {
 		if websocketAllowableOrigins == "" {
-			err, _ := psdLogger.Printf("Missing allowable Origin (-ws-origin=http://example.com)")
-			panic(err)
+			logger.Fatalf("Missing allowable Origin (-ws-origin=http://example.com)")
 		}
 
 		allowed := strings.Split(websocketAllowableOrigins, ",")
 		count := len(allowed)
 
 		if count > 1 {
-			err, _ := psdLogger.Fatalf("Only one origin server is supported at the moment")
-			panic(err)
+			logger.Fatalf("Only one origin server is supported at the moment")
 		}
 
 		websocketAllowableURLs = make([]url.URL, 0, count)
@@ -212,8 +208,7 @@ func main() {
 			url, err := url.Parse(test)
 
 			if err != nil {
-				err, _ := psdLogger.Printf("Invalid Origin parameter: %v, %v", test, err)
-				panic(err)
+				logger.Fatalf("Invalid Origin parameter: %v, %v", test, err)
 			}
 
 			websocketAllowableURLs = append(websocketAllowableURLs, *url)
@@ -244,15 +239,15 @@ func main() {
 	http.HandleFunc(websocketRoute, pubsocketdHandler)
 
 	if websocketAllowInsecure {
-		psdLogger.Printf("[init] listening for websocket requests on " + websocketEndpoint + websocketRoute + ", in INSECURE MODE which is not advised for production use")
+		logger.Printf("[init] listening for websocket requests on " + websocketEndpoint + websocketRoute + ", in INSECURE MODE which is not advised for production use")
 
 	} else {
-		psdLogger.Printf("[init] listening for websocket requests on " + websocketEndpoint + websocketRoute + ", from " + websocketAllowableOrigins)
+		logger.Printf("[init] listening for websocket requests on " + websocketEndpoint + websocketRoute + ", from " + websocketAllowableOrigins)
 	}
 
-	psdLogger.Printf("[init] listening for pubsub messages from " + redisEndpoint + " sent to the " + redisChannel + " channel")
+	logger.Printf("[init] listening for pubsub messages from " + redisEndpoint + " sent to the " + redisChannel + " channel")
 
 	if err := http.ListenAndServe(websocketEndpoint, nil); err != nil {
-		err, _ := psdLogger.Fatalf("Failed to start websocket server, because %v", err)
+		logger.Fatalf("Failed to start websocket server, because %v", err)
 	}
 }
